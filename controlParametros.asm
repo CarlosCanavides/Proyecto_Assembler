8ca05f178a61ce75b95dc7eb81f2526c9e5e07be
Carlos Canavides <cjcc1802@gmail.com>
2018-11-22 07:15:36 -0300

    Control de parametros finalizado.
    * El programa controla que los archivo de entrada y salida sean validos.
    * Ademas, dado que se crea un archivo temporal. Se controla que este no haya
      sido eliminado, antes del calculo de metricas.

--- controlParametros.asm
+++ controlParametros.asm
@@ -10,7 +10,6 @@
 %include "contador_metricas.asm"
 
 section .data
-	mensaje    db "hola "
 	fd_entrada dd 0                ; reserva memoria para mantener el descriptor del archivo de entrada.
 	fd_salida  dd 1                ; reserva memoria para mantener el descriptor del archivo de salida de los resultados. Por defecto es por consola.
 	file_temp  dd "temporal.txt"   ; nombre del archivo temporal, sera utilizado cuando la cantidad de parametros sea 0.
@@ -27,20 +26,21 @@ _start :
 	pop EBX              ; descartamos ARGV[0], nombre del programa.
 
 	cmp EAX,1            ; ¿La cantidad de parametros es igual a 0?
-	je ceroParametros    ; EAX = 0  ---> Rutina para 0 armumento.
+	je cero_parametros   ; EAX = 0  ---> Rutina para 0 armumento.
 
 	pop EBX              ; conservamos ARGV[1]
 
 	cmp EAX,2            ; ¿La cantidad de parametros es igual a 1?
-	je unParametro       ; EAX = 1  ---> Rutina para 1 armumento.
+	je un_parametro      ; EAX = 1  ---> Rutina para 1 armumento.
 
 	cmp EAX,3            ; ¿La cantidad de parametros es igual a 2?
-	je dosParametros     ; EAX = 2  ---> Rutina para 2 armumento.
+	je dos_parametros    ; EAX = 2  ---> Rutina para 2 armumento.
 
 	push terminacion_anormal  ; Si la cantidad de parametros no es la esperada, se produce una terminacion anormal.
-	jmp _exit
+	jmp _exit                 ; se ejecuta rutina de finalizacion.
 
-ceroParametros :
+
+cero_parametros :
 	mov  EAX, 8                 ; sys_creat
    	mov  EBX, file_temp         ; EBX = nombre del archivo temporal.
    	mov  ECX, 0777              ; permiso RWE para todos los usuarios.
@@ -48,12 +48,17 @@ ceroParametros :
 
    	mov [fd_entrada],EAX        ; fd_entrada = EAX;
 
-   	leerBuffer :
-		mov EAX, 3					; sys_read
+   	cmp EAX,0					; se verifica que el procesi "sys_creat" haya sido exitoso.
+   	jge leer_buffer				; si se pudo crear un nuevo archivo temporal, se procede a leer por consola.
+   	push terminacion_anormal    ; se parametriza la condicion de terminacion.
+   	jmp _exit                   ; se ejecuta la rutina de finalizacion.
+
+   	leer_buffer :
+		mov EAX, 3		            ; sys_read
 		mov EBX, 0                  ; indica ingreso por consola STDIN.
 		mov ECX, buffer             ; lo ingresado por usuario se almacenara en buffer.
 		mov EDX, 1000               ; EDX = longitud de lo que se leera por consola. 
-									; Se asume un maximo de 1000 caracteres por linea.
+							        ; Se asume un maximo de 1000 caracteres por linea.
 		int 0x80                    ; genera interrupcion.
 
 		cmp EAX,0                   ; Detecta la secuencia de escape para el fin del ingreso por teclado (Ctrl+D).
@@ -65,45 +70,85 @@ ceroParametros :
 		mov ECX, buffer             ; texto a escribir.
 		int 0x80                    ; genera interrupcion.
 
-		jmp leerBuffer              ; se vuelve a leer por consola.
+		jmp leer_buffer              ; se vuelve a leer por consola.
 
 	continuar :
-		push terminacion_normal  ; parametriza la condicion de terminacion.
-
 		mov EAX,6                ; sys_close
     	mov EBX,[fd_entrada]     ; indica puntero a archivo de entrada.
     	int 0x80                 ; genera interrrupcion.
 
 		mov EBX,file_temp		 ; EBX = nombre del archivo temporal.
-		call abrirArchivo        ; llamada a la rutina que abre el archivo mencionado en EBX.
+		call abrir_archivo       ; llamada a la rutina que abre el archivo mencionado en EBX.
 								 ; finalizada la rutina, el file_descriptor del archivo se localiza en EAX.
-
 		mov [fd_entrada],EAX     ; fd_entrada = EAX.
+		cmp EAX,0				 ; se verifica que el proceso "sys_open" haya sido exitoso.
+		jge procesar			 ; si se pudo abrir el archivo de temporal, continua la ejecucion.
 
-		call calcularMetricas    ; llamada a la rutina que calcula las metricas sobre el archivo temporal.
-		call cerrarArchivos      ; llamada a la rutina que se encarga de cerrar los archivos usados.
-		jmp _exit                ; se ejecuta rutina de finalizacion.
+		push terminacion_anormal ; se parametriza la condicion de terminacion.
+		jmp _exit                ; se ejecuta la rutina de finalizacion.
 
-unParametro  :
-	call abrirArchivo            ; llamada a la rutina que trata de abrir el archivo de entrada.
-	mov [fd_entrada],EAX         ; fd_entrada = EAX
-	call calcularMetricas        ; llamada a la rutina que calcula las metricas sobre el archivo de entrada.
-	jmp _exit                    ; se ejecuta la rutina de finalizacion.
 
-dosParametros :
-	call abrirArchivo            ; llamada a la rutina que trata de abrir el archivo de entrada.
-	mov [fd_entrada],EAX         ; fd_entrada = EAX
-	call calcularMetricas        ; llamada a la rutina que calcula las metricas sobre el archivo de entrada.
-	jmp _exit                    ; se ejecuta la rutina de finalizacion.
+un_parametro  :
+	call abrir_archivo                      ; llamada a la rutina que trata de abrir el archivo de entrada.
+	mov [fd_entrada],EAX                    ; fd_entrada = EAX
+	cmp EAX,0								; se verifica que el proceso "sys_open" haya sido exitoso.
+	jge procesar							; si se pudo abrir el archivo de entrada continua la ejecucion.
 
-abrirArchivo  :
+	push terminacion_anormal_archEntrada    ; se parametriza la condicion de terminacion.
+	jmp _exit                               ; se ejecuta la rutina de finalizacion.
+
+
+dos_parametros :
+	call abrir_archivo                      ; llamada a la rutina que trata de abrir el archivo de entrada.
+	mov [fd_entrada],EAX         			; fd_entrada = EAX
+	cmp EAX,0								; se verifica que el proceso "sys_open" haya sido exitoso.
+	jge abrir_archSalida					; si se pudo abrir el archivo de entrada se procede a abrir el archivo de salida.
+
+	push terminacion_anormal_archEntrada	; se parametriza la condion de terminacion.
+	jmp _exit								; se ejecuta la rutina de finalizacion.
+
+	abrir_archSalida :
+		pop EBX								; se desapila es segundo parametro ARGV[2], que corresponde con el archivo de salida.
+		call abrir_archivo_escritura		; llamada a la rutina que trata de abrir el archivo de salida.
+		mov [fd_salida],EAX					; fd_salida = EAX
+		cmp EAX,0							; se verifica que el proceso "sys_open" haya sido exitoso.
+		jge procesar						; si se pudo abrir el archivo de salida, continua la ejecucion.
+
+		push terminacion_anormal_archSalida ; se parametriza la condicion de terminacion.
+		jmp _exit							; se ejecuta la rutina de finalizacion.
+
+
+; Una vez que el control de los parametros fue realizado, esta rutina se encarga del procesamiento posterior.
+; Asume que todo los archivos correspondientes pudieron abrirse exitosamente.
+procesar :
+	call calcular_metricas     ; llamada a la rutina que calcula las metricas sobre el archivo de entrada.
+	call mostrar_resultados    ; llamada a la rutina que se encarga de mostrar los resultados.
+	call cerrar_archivos       ; llamada a la rutina que se encarga de cerrar los archivos usados.
+	push terminacion_normal    ; parametriza la condicion de terminacion.
+	jmp _exit   			   ; se ejecuta la rutina de finalizacion.
+
+
+; Rutina que se encarga de abrir un archivo para lectura, cuyo file_name ya se encuentra en el registro EBX.
+abrir_archivo  :
     mov EAX,5                  ; sys_open
     mov ECX,0                  ; modo lectura, para el archivo.  
 	mov EDX,0777               ; permiso RWE para todos los usuarios.
 	int 0x80                   ; genera interrupcion.
-	ret                        ; retorno
+	ret                    	   ; retorno
 
-cerrarArchivos :
+
+; Rutina que se encarga de abrir un archivo para escritura, cuyo file_name ya se encuentra en el registro EBX.
+abrir_archivo_escritura :
+	mov EAX,5                  ; sys_open
+    mov ECX,1                  ; modo escritura, para el archivo.  
+	mov EDX,0777               ; permiso RWE para todos los usuarios.
+	int 0x80                   ; genera interrupcion.
+	ret                    	   ; retorno
+
+
+; Rutina que se encarga de cerrar los archivos de entrada y salida.
+; Si el archivo de salida es "salida estandar" no necesita cerrarlo.
+cerrar_archivos :
 	mov EAX,6                  ; sys_close
     mov EBX,[fd_entrada]       ; indica puntero a archivo de entrada.
     int 0x80                   ; genera interrrupcion.
@@ -119,7 +164,10 @@ cerrarArchivos :
     fin :
     	ret 				   ; retorno
 
-calcularMetricas :
+
+; Rutina que se encarga de procesar el archivo de entrada y calcular las metricas.
+; Recibe un file_descriptor en el registro EBX.
+calcular_metricas :
 	leer_linea :
 		mov EAX,3                  ; sys_read
 		mov EBX,[fd_entrada]	   ; EBX = archivo de entrada.
@@ -141,7 +189,6 @@ calcularMetricas :
 		mov cl, byte[buffer+EBX]   ; ECX = caracter contenido en el buffer.
 		dec EAX                    ; decremento la cantidad de caracteres que faltan procesar.
 		call analizar_caracter     ; llamada a la rutina que analiza el caracter leido.
-		call println			   ; Dev only
 		cmp EAX,0                  ; comparo la cantidad de caracteres que faltan procesar con 0.
 		je leer_linea              ; si ya no hay nada mas para procesar, vuelvo a leer otra linea.
 		inc EBX                    ; si faltan procesar caracteres, incremento el registro de desplazamiento.
@@ -155,21 +202,30 @@ _exit :
 	pop EBX     ; condicion de terminacion.
 	int 0x80    ; genera interrupcion.
 
-println :
-	; Dev only
-	push EAX
-	push EBX
-	push ECX
-
+mostrar_resultados :
 	mov EAX, 4
-	mov EBX, 1
-	mov ECX, mensaje
-	mov EDX, 5
+	mov EBX, [fd_salida]
+	mov ECX, cant_letras
+	mov EDX, 1
 	int 0x80
 
-	pop ECX
-	pop EBX
-	pop EAX
+	mov EAX, 4
+	mov EBX, [fd_salida]
+	mov ECX, cant_palabras
+	mov EDX, 1
+	int 0x80
+
+	mov EAX, 4
+	mov EBX, [fd_salida]
+	mov ECX, cant_lineas
+	mov EDX, 1
+	int 0x80
+
+	mov EAX, 4
+	mov EBX, [fd_salida]
+	mov ECX, cant_parrafos
+	mov EDX, 1
+	int 0x80
 
 	ret
 
